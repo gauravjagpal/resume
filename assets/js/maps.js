472d20115f78e8718fa487143a4a3118003e55df
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-07-27 19:43:07 +0100

    moved script tags to js files

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,8 +10,16 @@ function initMap() {
 var labels = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
 
 var locations = [
-    { lat: 45.1000, lng: 15.2},
-    { lat: 32.7607, lng: 32.7607}
+    { lat: 45.1000, lng: 15.7607},
+    { lat: 32.6669, lng: -16.9240},
+    { lat: 35.1264, lng: 33.4299},
+    { lat: 41.3874, lng: 2.1686},
+    { lat: 4.2105, lng: 101.9758},
+    { lat: 1.3521, lng: 103.8198},
+    { lat: 40.7128, lng: -74.0060},
+    { lat: 41.8719, lng: 12.5674},
+    { lat: 20.5937, lng: 78.9629},
+    { lat: 35.2401, lng: 24.8093},
 ];
 
 var markers = locations.map(function(location, i) {
@@ -21,7 +29,6 @@ var markers = locations.map(function(location, i) {
     })
 });
 
-var markerClusterer = MarkerClusterer(map, markers,
-    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
 }
